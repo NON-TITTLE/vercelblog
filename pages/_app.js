d83aa58029c26c5adcd643b1d3f8a3fd6d188d60
Pizza-Dev <189063880+NON-TITTLE@users.noreply.github.com>
2024-12-14 05:56:10 -0600

    Versión lista para producción

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,3 @@
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
-import { VercelToolbar } from '@vercel/toolbar/next';
-
-export default function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Component {...pageProps} />
-            <VercelToolbar />
-        </>
-    );
-}
