8566bb8680fb9b412741de789488c9a4684f2787
Pizza-Dev <189063880+NON-TITTLE@users.noreply.github.com>
2024-12-14 05:45:46 -0600

    Añadida Vercel Toolbar para desarrollo

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,13 @@
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
+import { VercelToolbar } from '@vercel/toolbar/next';
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Component {...pageProps} />
+            <VercelToolbar />
+        </>
+    );
+}
